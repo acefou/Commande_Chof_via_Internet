a35efc46973fa42f3b11be37318f2bbdd458aafe
acefou <35888565+acefou@users.noreply.github.com>
2018-05-13 22:18:56 +0200

    Add files via upload

--- envoicmd.php
+++ envoicmd.php
@@ -1,12 +1,18 @@
  <?php
 
 include_once 'quelip.php';
+include 'myfonctions/fenvoimail.php';
+
 $lip=fquelip();
 //active commande
 $Ladresse = "http://".$lip."/handleremcmdchof";
 $lines = file($Ladresse);
 $Lestatus = (int)$lines[0];
 
+$mail_Data .= "Demande de changement de l'état du relais de chauffage via internet \r\n";
+$mail_Data .= "Vérifier l'état.\r\n";
+
+$CR_Mail = envoimail("Activation a distance", $mail_Data, 1);
 
 //determination du bouton
 switch ($Lestatus) {
